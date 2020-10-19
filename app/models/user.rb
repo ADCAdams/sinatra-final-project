42e10203caca741c964dcee18c72980056fb1d1b
adcadams <andrewdcadams@gmail.com>
2020-10-19 18:41:16 -0400

    add uniqueness to username attribuate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_many :albums
     has_many :songs, through: :albums
-    validates_presence_of :username
+    validates :username, presence: true, uniqueness: true
     has_secure_password
 end
